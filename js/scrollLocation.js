4775dae54ac4601fdb39ecae048545bd641d6ede
Bastola <messiniki@outlook.com>
2023-05-28 08:39:10 +0545

    Nav Menu Button Link Changed

---
+++ js/scrollLocation.js
@@ -0,0 +1,9 @@
+function scrollView() {
+    let element = document.getElementById("mapContainer");
+    element.scrollIntoView();
+}
+
+function scrollIntoFooter() {
+    let footerElem = document.getElementById("footerContainer");
+    footerElem.scrollIntoView();
+}
